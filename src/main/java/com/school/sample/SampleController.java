75897069a49aebb023a1c86a988df15667e9e570
Florin Chascear <153073613+NYXx0p@users.noreply.github.com>
2024-05-16 20:04:09 +0100

    InterfaceFeita

--- src/main/java/com/school/sample/SampleController.java
+++ src/main/java/com/school/sample/SampleController.java
@@ -3,10 +3,66 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class SampleController {
+    public AnchorPane ClientesTela;
+    public AnchorPane FornecedoresTela;
+    public AnchorPane SobreMimTela;
     @FXML
-    private TextField txtDisciplina;
+    private GridPane produtos_grid;
+
     @FXML
-    private Button btnTeste;
+    private AnchorPane HomeTela;
+
+    @FXML
+    private AnchorPane ProdutosTela;
+
+    @FXML
+    void ClientesButton(ActionEvent event) {
+        HomeTela.setVisible(false);
+        ProdutosTela.setVisible(false);
+        ClientesTela.setVisible(true);
+        FornecedoresTela.setVisible(false);
+        SobreMimTela.setVisible(false);
+    }
+
+    @FXML
+    void FornecedoresButton(ActionEvent event) {
+        HomeTela.setVisible(false);
+        ProdutosTela.setVisible(false);
+        ClientesTela.setVisible(false);
+        FornecedoresTela.setVisible(true);
+        SobreMimTela.setVisible(false);
+    }
+
+    @FXML
+    void HomeButton(ActionEvent event) {
+        HomeTela.setVisible(true);
+        ProdutosTela.setVisible(false);
+        ClientesTela.setVisible(false);
+        FornecedoresTela.setVisible(false);
+        SobreMimTela.setVisible(false);
+    }
+
+    @FXML
+    void ProdutosButton(ActionEvent event) {
+        HomeTela.setVisible(false);
+        ProdutosTela.setVisible(true);
+        ClientesTela.setVisible(false);
+        FornecedoresTela.setVisible(false);
+        SobreMimTela.setVisible(false);
+    }
+
+
+    @FXML
+    void SobreButton(ActionEvent event) {
+        HomeTela.setVisible(false);
+        ProdutosTela.setVisible(false);
+        ClientesTela.setVisible(false);
+        FornecedoresTela.setVisible(false);
+        SobreMimTela.setVisible(true);
+    }
+
 }
